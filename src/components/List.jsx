12a6c5b88ba4008bf581b5c1c3c7e012a67d9d02
minxng <minxng12@gmail.com>
2024-07-10 23:47:32 +0900

    리스트 channel 필터링

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,11 +8,13 @@ export default function List({ searchParam }) {
       `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchParam}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
     )
       .then((resp) => resp.json())
-      .then((data) => setVideo(data.items));
+      .then((data) => {
+        setVideo(data.items.filter((item) => item.id.kind === "youtube#video"));
+      });
   }, [searchParam]);
 
   return (
-    <section className="grid grid-cols-4 p-20">
+    <section className="grid grid-cols-4 py-4 gap-x-6">
       {video.map((v) => (
         <VideoBox data={v} key={v.id.videoId} />
       ))}
