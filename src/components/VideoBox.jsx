5f10555cfb9cdc94ae4c43376d9dc375a62ffc87
minxng <minxng12@gmail.com>
2024-07-10 23:55:18 +0900

    list 노출 데이터 변경

--- src/components/VideoBox.jsx
+++ src/components/VideoBox.jsx
@@ -3,28 +3,22 @@ import { Link } from "react-router-dom";
 
 export default function VideoBox({ data }) {
   return (
-    <div key={data.id.videoId}>
-      <div className="w-1/2">
-        <iframe
-          id="ytplayer"
-          type="text/html"
-          title={data.id.videoId}
-          src={`https://www.youtube.com/embed/${data.id.videoId}`}
-          allowFullScreen
-          className="rounded-md w-full"
-        />
-      </div>
-      <p className="flex flex-col">
-        <Link
-          to={data.id.videoId}
-          state={{ data }}
-          className="text-white text-ellipsis overflow-hidden w-full break-words line-clamp-2"
-        >
-          {data.snippet.title}
-        </Link>
-        <span className="text-gray-300">{data.snippet.channelTitle}</span>
-        <span className="text-gray-300">{data.snippet.publishedAt}</span>
-      </p>
-    </div>
+    <Link
+      to={data.id.videoId}
+      state={data.snippet}
+      className="flex flex-col"
+      key={data.id.videoId}
+    >
+      <img
+        src={data.snippet.thumbnails.medium.url}
+        alt="thumbnail"
+        className="rounded-lg"
+      />
+      <span className="text-ellipsis overflow-hidden w-full break-words line-clamp-2">
+        {data.snippet.title}
+      </span>
+      <span className="text-gray-500">{data.snippet.channelTitle}</span>
+      <span className="text-gray-500">{data.snippet.publishedAt}</span>
+    </Link>
   );
 }
